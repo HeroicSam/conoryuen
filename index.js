20a02d31f5a5e1b28fe9edd72128f9dd58f170ba
HeroicSam <cyuen10@gmail.com>
2022-06-02 20:07:16 -0400

    added new pages + js file. change page functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+function changePageHome() {
+    window.location.href="../home.html"
+}
+
+function changePageAbout() {
+    window.location.href="./about.html"
+}
+
+function changePagePortfolio() {
+    window.location.href="./portfolio.html"
+}
+
+function changePageContact() {
+    window.location.href="./contact.html"
+}
