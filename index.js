3724f9524e7bcf976b5bd1281582a50c405b6e92
HeroicSam <cyuen10@gmail.com>
2022-06-05 23:03:53 -0400

    reorganized css file, preparing to add new animations

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-function changePageHome() {
-    window.location.href="../home.html"
-}
+// Change page functions
+// function changePageHome() {
+//     window.location.href="../home.html"
+// }
 
-function changePageAbout() {
-    window.location.href="./about.html"
-}
+// function changePageAbout() {
+//     window.location.href="./about.html"
+// }
 
-function changePagePortfolio() {
-    window.location.href="./portfolio.html"
-}
+// function changePagePortfolio() {
+//     window.location.href="./portfolio.html"
+// }
 
-function changePageContact() {
-    window.location.href="./contact.html"
-}
+// function changePageContact() {
+//     window.location.href="./contact.html"
+// }
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const header = document.querySelector('header');
+const sectionOne = document.querySelector('main');
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries, 
+    sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                header.classList.add('nav-scrolled');
+            } else {
+                header.classList.remove('nav-scrolled');
+            }
+        });
+
+}, 
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
